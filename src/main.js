e2e0af2356b3568d89bb6619e88b45661f3f41c1
zhouzhenhan <zhouzhenhan@example.com>
2022-05-10 10:10:18 +0800

    newmain.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import inputSearch from './components/inputSearch'
 
+const components = [
+  inputSearch,
+]
+const install = function (Vue) {
+  if(install.installed) return;
+  components.map(component =>Vue.components(component.name, component))
+
+  if(typeof window !=="undefined" && window.Vue){
+    install(window.Vue)
+  }
+}
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+export default  install;
